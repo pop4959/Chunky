cc0f957a37d2dd8670d77b450e2b403b916761b9
JakeTS <timpbh@gmail.com>
2020-08-08 17:41:01 -0400

    The spiral chunk coord iterator now can start at any place.

--- src/main/java/org/popcraft/chunky/SpiralChunkCoordinateIterator.java
+++ src/main/java/org/popcraft/chunky/SpiralChunkCoordinateIterator.java
@@ -18,14 +18,14 @@ public class SpiralChunkCoordinateIterator implements ChunkCoordinateIterator {
     private boolean xNotZ = true;
 
     public SpiralChunkCoordinateIterator(int radius, int centerX, int centerZ, int startCount) {
-        if (startCount < 0) throw new IllegalArgumentException("Start count must be 1 or greater.");
+        if (startCount < 0) throw new IllegalArgumentException("Start count must be 0 or greater.");
         diameterChunks = radius / 8 + (radius % 8 != 0 ? 1 : 0);
         posX = centerX >> 4;
         posZ = centerX >> 4;
         // If we are starting at the first position, we are already done.
-        if (startCount == 1) return;
+        if (startCount == 0) return;
         // Our internal count starts at 0. The count that we display starts at 1.
-        count = startCount - 1;
+        count = startCount;
         // Stretch is basically the inverse of { aₙ = n²-n+2 } offset by one and floored.
         // This value is useful to us for other things as well though, so we save it as its own thing.
         stretch = (int) (0.5 + Math.sqrt(1d - 4d * (2 - startCount - 1)) / 2d);
@@ -41,14 +41,14 @@ public class SpiralChunkCoordinateIterator implements ChunkCoordinateIterator {
             posX += -1 + (stretch - 1) / 2;
             posZ = (stretch - 1) / 2;
         } else {
-            posX += -(stretch / 2 - 1);
-            posZ += -1 - (stretch / 2 - 1);
+            posX += -stretch / 2 + 1;
+            posZ += -1 - stretch / 2 + 1;
         }
         if (xNotZ) {
             posX += flip * (subCount - 1);
         } else {
             posX += flip * (stretch - 1);
-            posZ += flip * (subCount);
+            posZ += flip * subCount;
         }
     }
 
