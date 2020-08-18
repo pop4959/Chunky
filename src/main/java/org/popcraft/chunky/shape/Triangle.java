f8fbdfea2ea024d15bdc00f19cdcdca91a5d201c
pop4959 <pop4959@gmail.com>
2020-08-18 11:23:28 -0700

    Refactor Triangle

--- src/main/java/org/popcraft/chunky/shape/Triangle.java
+++ src/main/java/org/popcraft/chunky/shape/Triangle.java
@@ -4,17 +4,22 @@ import org.popcraft.chunky.ChunkCoordinate;
 import org.popcraft.chunky.iterator.ChunkIterator;
 
 public class Triangle extends AbstractShape {
+    private int p1x, p1z, p2x, p2z, p3x, p3z;
+
     public Triangle(ChunkIterator chunkIterator) {
         super(chunkIterator);
+        this.p1x = radius;
+        this.p1z = radius;
+        this.p2x = -radius;
+        this.p2z = radius;
+        this.p3x = 0;
+        this.p3z = -radius;
     }
 
     @Override
     public boolean isBounding(ChunkCoordinate chunkCoordinate) {
         int xChunk = (chunkCoordinate.x << 4) + 8;
         int zChunk = (chunkCoordinate.z << 4) + 8;
-        int p1x = radius, p1z = radius;
-        int p2x = -radius, p2z = radius;
-        int p3x = 0, p3z = -radius;
         boolean insideLp1p2 = insideLine(p1x, p1z, p2x, p2z, xChunk, zChunk);
         boolean insideLp2p3 = insideLine(p2x, p2z, p3x, p3z, xChunk, zChunk);
         boolean insideLp3p1 = insideLine(p3x, p3z, p1x, p1z, xChunk, zChunk);
