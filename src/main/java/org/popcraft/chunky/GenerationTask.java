f433c4b9f1f931fd509d827f4f5073dfe805e441
pop4959 <pop4959@gmail.com>
2020-08-19 16:34:07 -0700

    More descriptive thread name

--- src/main/java/org/popcraft/chunky/GenerationTask.java
+++ src/main/java/org/popcraft/chunky/GenerationTask.java
@@ -107,7 +107,7 @@ public class GenerationTask implements Runnable {
     @Override
     public void run() {
         final String poolThreadName = Thread.currentThread().getName();
-        Thread.currentThread().setName("Generation Task Thread");
+        Thread.currentThread().setName(String.format("Chunky-%s Thread", world.getName()));
         final Semaphore working = new Semaphore(MAX_WORKING);
         startTime.set(System.currentTimeMillis());
         while (!stopped && chunkIterator.hasNext()) {
