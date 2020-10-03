1d13e2c12efc753a11ef0d01d252b9a0768fc220
Dylan Xaldin <Puremin0rez515@gmail.com>
2020-10-03 01:40:54 -0500

    Provide a better message if trying to cancel with no tasks (#39)
    This changes the cancel command to properly inform the user if any tasks were actually cancelled, rather than just always saying that all tasks were cancelled.

--- src/main/java/org/popcraft/chunky/command/CancelCommand.java
+++ src/main/java/org/popcraft/chunky/command/CancelCommand.java
@@ -9,6 +9,10 @@ public class CancelCommand extends ChunkyCommand {
     }
 
     public void execute(CommandSender sender, String[] args) {
+        if (chunky.getGenerationTasks().size() == 0 && chunky.getConfigStorage().loadTasks().size() == 0) {
+            sender.sendMessage(chunky.message("format_cancel_no_tasks", chunky.message("prefix")));
+            return;
+        }
         sender.sendMessage(chunky.message("format_cancel", chunky.message("prefix")));
         chunky.getConfigStorage().cancelTasks();
         chunky.getGenerationTasks().values().forEach(generationTask -> generationTask.stop(true));
