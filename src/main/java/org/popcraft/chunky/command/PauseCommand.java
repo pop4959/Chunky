fc86380b613c5d6bc47d47a4d9a906c3d42ccb76
Dylan Xaldin <Puremin0rez515@gmail.com>
2020-10-03 02:42:31 -0500

    Provide a better message if trying to pause with no tasks (#41)
    This changes the pause command to properly inform the user if there were no tasks to pause, rather than returning nothing and leaving the user with no command feedback whatsoever.

--- src/main/java/org/popcraft/chunky/command/PauseCommand.java
+++ src/main/java/org/popcraft/chunky/command/PauseCommand.java
@@ -10,6 +10,10 @@ public class PauseCommand extends ChunkyCommand {
     }
 
     public void execute(CommandSender sender, String[] args) {
+        if (chunky.getGenerationTasks().size() == 0) {
+            sender.sendMessage(chunky.message("format_pause_no_tasks", chunky.message("prefix")));
+            return;
+        }
         for (GenerationTask generationTask : chunky.getGenerationTasks().values()) {
             generationTask.stop(false);
             sender.sendMessage(chunky.message("format_pause", chunky.message("prefix"), generationTask.getWorld().getName()));
