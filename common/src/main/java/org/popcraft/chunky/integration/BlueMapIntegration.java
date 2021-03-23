8435db73afecff598104e092c61ef48e455afd48
pop4959 <pop4959@gmail.com>
2021-03-22 21:22:19 -0700

    Update to BlueMap API 1.3

--- common/src/main/java/org/popcraft/chunky/integration/BlueMapIntegration.java
+++ common/src/main/java/org/popcraft/chunky/integration/BlueMapIntegration.java
@@ -2,11 +2,9 @@ package org.popcraft.chunky.integration;
 
 import com.flowpowered.math.vector.Vector2d;
 import de.bluecolored.bluemap.api.BlueMapAPI;
-import de.bluecolored.bluemap.api.BlueMapAPIListener;
 import de.bluecolored.bluemap.api.marker.MarkerAPI;
 import de.bluecolored.bluemap.api.marker.MarkerSet;
 import de.bluecolored.bluemap.api.marker.ShapeMarker;
-import org.popcraft.chunky.Chunky;
 import org.popcraft.chunky.platform.World;
 import org.popcraft.chunky.shape.AbstractEllipse;
 import org.popcraft.chunky.shape.AbstractPolygon;
@@ -16,33 +14,25 @@ import java.awt.Color;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Consumer;
 
-public class BlueMapIntegration extends AbstractMapIntegration implements BlueMapAPIListener {
-    private Chunky chunky;
+public class BlueMapIntegration extends AbstractMapIntegration {
     private BlueMapAPI blueMapAPI;
-    private List<Consumer<Void>> toAdd = new ArrayList<>();
+    private List<Runnable> pendingMarkers = new ArrayList<>();
+    private static final String MARKERSET_ID = "chunky";
 
-    public BlueMapIntegration(Chunky chunky) {
-        this.chunky = chunky;
-    }
-
-    @Override
-    public void onEnable(BlueMapAPI blueMapApi) {
-        this.blueMapAPI = blueMapApi;
-        toAdd.forEach(add -> add.accept(null));
-        toAdd.clear();
-    }
-
-    @Override
-    public void onDisable(BlueMapAPI blueMapApi) {
-        this.blueMapAPI = null;
+    public BlueMapIntegration() {
+        BlueMapAPI.onEnable(blueMap -> {
+            this.blueMapAPI = blueMap;
+            pendingMarkers.forEach(Runnable::run);
+            pendingMarkers.clear();
+        });
+        BlueMapAPI.onDisable(blueMap -> this.blueMapAPI = null);
     }
 
     @Override
     public void addShapeMarker(final World world, final Shape shape) {
         if (blueMapAPI == null) {
-            this.toAdd.add(v -> this.addShapeMarker(world, shape));
+            this.pendingMarkers.add(() -> this.addShapeMarker(world, shape));
             return;
         }
         final MarkerAPI markerAPI;
@@ -51,7 +41,8 @@ public class BlueMapIntegration extends AbstractMapIntegration implements BlueMa
         } catch (IOException e) {
             return;
         }
-        final MarkerSet markerSet = markerAPI.createMarkerSet(this.label.toLowerCase());
+        final MarkerSet markerSet = markerAPI.createMarkerSet(MARKERSET_ID);
+        markerSet.setLabel(this.label);
         de.bluecolored.bluemap.api.marker.Shape blueShape;
         if (shape instanceof AbstractPolygon) {
             AbstractPolygon polygon = (AbstractPolygon) shape;
@@ -70,14 +61,14 @@ public class BlueMapIntegration extends AbstractMapIntegration implements BlueMa
             double[] center = ellipse.getCenter();
             double[] radii = ellipse.getRadii();
             Vector2d centerPos = new Vector2d(center[0], center[1]);
-            blueShape = createEllipse(centerPos, radii[0], radii[1], 100);
+            blueShape = de.bluecolored.bluemap.api.marker.Shape.createEllipse(centerPos, radii[0], radii[1], 100);
         } else {
             return;
         }
         blueMapAPI.getWorld(world.getUUID()).ifPresent(blueWorld -> blueWorld.getMaps().forEach(map -> {
-            ShapeMarker marker = markerSet.createShapeMarker(shapeLabel(world), map, blueShape, world.getSeaLevel());
+            ShapeMarker marker = markerSet.createShapeMarker(world.getName(), map, blueShape, world.getSeaLevel());
             marker.setColors(this.color, new Color(0, true));
-            marker.setLabel(this.label.toLowerCase());
+            marker.setLabel(this.label);
         }));
         try {
             markerAPI.save();
@@ -86,15 +77,15 @@ public class BlueMapIntegration extends AbstractMapIntegration implements BlueMa
     }
 
     @Override
-    public void removeShapeMarker(World world) {
+    public void removeShapeMarker(final World world) {
         if (blueMapAPI == null) {
             return;
         }
         try {
             final MarkerAPI markerAPI = blueMapAPI.getMarkerAPI();
-            final MarkerSet markerSet = markerAPI.createMarkerSet(this.label.toLowerCase());
+            final MarkerSet markerSet = markerAPI.createMarkerSet(MARKERSET_ID);
             blueMapAPI.getWorld(world.getUUID()).ifPresent(blueWorld -> blueWorld.getMaps().forEach(map -> {
-                markerSet.removeMarker(shapeLabel(world));
+                markerSet.removeMarker(world.getName());
             }));
             markerAPI.save();
         } catch (IOException ignored) {
@@ -108,26 +99,10 @@ public class BlueMapIntegration extends AbstractMapIntegration implements BlueMa
         }
         try {
             final MarkerAPI markerAPI = blueMapAPI.getMarkerAPI();
-            final MarkerSet markerSet = markerAPI.createMarkerSet(this.label.toLowerCase());
-            chunky.getPlatform().getServer().getWorlds().forEach(world -> markerSet.removeMarker(shapeLabel(world)));
-            markerAPI.save();
+            if (markerAPI.removeMarkerSet(MARKERSET_ID)) {
+                markerAPI.save();
+            }
         } catch (IOException ignored) {
         }
-    }
-
-    private String shapeLabel(World world) {
-        return String.format("%s.%s", "chunky.markerset", world.getName());
-    }
-
-    private de.bluecolored.bluemap.api.marker.Shape createEllipse(Vector2d centerPos, double radiusX, double radiusZ, @SuppressWarnings("SameParameterValue") int points) {
-        if (points < 3) throw new IllegalArgumentException("A shape has to have at least 3 points!");
-        Vector2d[] pointArray = new Vector2d[points];
-        double segmentAngle = 2 * Math.PI / points;
-        double angle = 0d;
-        for (int i = 0; i < points; i++) {
-            pointArray[i] = centerPos.add(Math.sin(angle) * radiusX, Math.cos(angle) * radiusZ);
-            angle += segmentAngle;
-        }
-        return new de.bluecolored.bluemap.api.marker.Shape(pointArray);
     }
 }
