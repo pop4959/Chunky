8266ca300c10130e36ffc1bb5a744596952f184f
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2021-07-18 20:58:12 +0100

    final commit hopefully fixes the StatusCommand

--- common/src/main/java/org/popcraft/chunky/command/StatusCommand.java
+++ common/src/main/java/org/popcraft/chunky/command/StatusCommand.java
@@ -3,12 +3,11 @@ package org.popcraft.chunky.command;
 import org.popcraft.chunky.Chunky;
 import org.popcraft.chunky.GenerationTask;
 import org.popcraft.chunky.platform.Sender;
+import org.popcraft.chunky.platform.World;
 
 import java.util.List;
+import java.util.Map;
 
-/**
- * @author Jamalam360
- */
 public class StatusCommand extends ChunkyCommand {
     public StatusCommand(Chunky chunky) {
         super(chunky);
@@ -16,7 +15,7 @@ public class StatusCommand extends ChunkyCommand {
 
     @Override
     public void execute(Sender sender, String[] args) {
-        final List<GenerationTask> tasks = chunky.getConfig().loadTasks();
-        tasks.forEach((generationTask -> generationTask.getProgress().sendUpdate(sender)));
+        final Map<World, GenerationTask> tasks = chunky.getGenerationTasks();
+        tasks.forEach(((world, generationTask) -> generationTask.getProgress().sendUpdate(sender)));
     }
 }
