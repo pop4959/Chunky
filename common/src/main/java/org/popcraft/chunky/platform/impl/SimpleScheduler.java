5a85972030650db6c8f36ddbd77513f04d05c0e9
Dylan Xaldin <Puremin0rez515@gmail.com>
2021-08-02 03:14:59 -0500

    [Common] Fix SimpleScheduler behavior (#117)

--- common/src/main/java/org/popcraft/chunky/platform/impl/SimpleScheduler.java
+++ common/src/main/java/org/popcraft/chunky/platform/impl/SimpleScheduler.java
@@ -3,7 +3,7 @@ package org.popcraft.chunky.platform.impl;
 import org.popcraft.chunky.platform.Scheduler;
 
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
@@ -12,12 +12,14 @@ public class SimpleScheduler implements Scheduler {
     private final ThreadGroup tasks = new ThreadGroup("tasks");
 
     public SimpleScheduler() {
-        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(3, Integer.MAX_VALUE, 5, TimeUnit.MINUTES, new LinkedBlockingQueue<>());
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(3, Integer.MAX_VALUE, 5, TimeUnit.MINUTES, new SynchronousQueue<>());
         threadPoolExecutor.setThreadFactory((runnable) -> {
             Thread thread = new Thread(tasks, runnable);
             thread.setDaemon(true);
             return thread;
         });
+        threadPoolExecutor.prestartAllCoreThreads();
+        threadPoolExecutor.allowCoreThreadTimeOut(true);
         this.executor = threadPoolExecutor;
     }
 
