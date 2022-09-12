2b8ce944b32fcc656b10792fd231c824567c3131
pop4959 <pop4959@gmail.com>
2022-09-11 22:43:09 -0700

    Update javadoc

---
+++ docs/chunky/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.popcraft.chunky.api"},{"l":"org.popcraft.chunky.api.event.task"}];updateSearchResults();
---
+++ docs/chunky/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.popcraft.chunky.api","l":"ChunkyAPI"},{"p":"org.popcraft.chunky.api.event.task","l":"GenerationCompleteEvent"},{"p":"org.popcraft.chunky.api.event.task","l":"GenerationProgressEvent"}];updateSearchResults();
