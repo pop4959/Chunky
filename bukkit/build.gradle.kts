75640077b44e979c9feb55f67d3bf6116a3493fb
Christopher Bohn <pop4959@gmail.com>
2023-03-02 00:45:29 -0800

    Update Paper repo URL

--- bukkit/build.gradle.kts
+++ bukkit/build.gradle.kts
@@ -1,6 +1,6 @@
 repositories {
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
-    maven("https://papermc.io/repo/repository/maven-public")
+    maven("https://repo.papermc.io/repository/maven-public/")
     maven("https://jitpack.io")
 }
 
