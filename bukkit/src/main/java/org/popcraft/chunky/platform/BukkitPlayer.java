580f0072e3097fd294137a689abbbad81c28213c
Metriximor <metriximor@gmail.com>
2023-11-22 07:28:53 +0000

    Improve vehicle teleporting logic (#301)

--- bukkit/src/main/java/org/popcraft/chunky/platform/BukkitPlayer.java
+++ bukkit/src/main/java/org/popcraft/chunky/platform/BukkitPlayer.java
@@ -4,8 +4,12 @@ import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.popcraft.chunky.ChunkyBukkit;
 import org.popcraft.chunky.platform.util.Location;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
@@ -13,6 +17,7 @@ import static org.popcraft.chunky.util.Translator.translateKey;
 
 public class BukkitPlayer extends BukkitSender implements Player {
     private static final boolean ACTION_BAR_SUPPORTED;
+    private final JavaPlugin plugin = JavaPlugin.getPlugin(ChunkyBukkit.class);
 
     static {
         boolean barSupported;
@@ -58,18 +63,28 @@ public class BukkitPlayer extends BukkitSender implements Player {
         return player.getUniqueId();
     }
 
+    @SuppressWarnings("UnstableApiUsage")
     @Override
     public void teleport(final Location location) {
         final org.bukkit.World world = Bukkit.getWorld(location.getWorld().getName());
         final org.bukkit.Location loc = new org.bukkit.Location(world, location.getX(), location.getY(), location.getZ(), location.getYaw(), location.getPitch());
         final Entity vehicle = player.getVehicle();
-        teleportAsync(player, loc);
-        if (vehicle != null) {
-            teleportAsync(vehicle, loc).thenAccept(success -> {
-                if (Boolean.TRUE.equals(success)) {
-                    vehicle.addPassenger(player);
+        if (vehicle == null) {
+            teleportAsync(player, loc);
+        } else {
+            final List<Entity> passengers = new ArrayList<>(vehicle.getPassengers());
+            vehicle.eject();
+            teleportAsync(player, loc).thenAcceptAsync(ignored -> {
+                vehicle.teleport(player);
+                for (final Entity passenger : passengers) {
+                    passenger.teleport(player);
+                    if (passenger instanceof final org.bukkit.entity.Player playerPassenger) {
+                        playerPassenger.hideEntity(plugin, vehicle);
+                        playerPassenger.showEntity(plugin, vehicle);
+                    }
+                    vehicle.addPassenger(passenger);
                 }
-            });
+            }, command -> plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, command, 1));
         }
     }
 
