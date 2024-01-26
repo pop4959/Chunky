f59d7c47598fb6bace5dea96e3c7fbc66363b8f8
Christopher Bohn <pop4959@gmail.com>
2024-01-25 23:07:56 -0800

    Revert "Adjust max working count default scaling"
    This reverts commit 9e447d4a2ad62ea1e50caed39739e622de401d93.

--- common/src/main/java/org/popcraft/chunky/GenerationTask.java
+++ common/src/main/java/org/popcraft/chunky/GenerationTask.java
@@ -22,8 +22,7 @@ import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class GenerationTask implements Runnable {
-    private static final int DEFAULT_WORKING_COUNT = 50 * Math.min(Runtime.getRuntime().availableProcessors(), 10);
-    private static final int MAX_WORKING_COUNT = Input.tryInteger(System.getProperty("chunky.maxWorkingCount")).orElse(DEFAULT_WORKING_COUNT);
+    private static final int MAX_WORKING_COUNT = Input.tryInteger(System.getProperty("chunky.maxWorkingCount")).orElse(50);
     private static final double SAMPLE_INTERVAL = 1000d * Math.max(Input.tryInteger(System.getProperty("chunky.sampleInterval")).orElse(30), 30);
     private static final double SAMPLE_SUB_INTERVAL = SAMPLE_INTERVAL / 30;
     private final Chunky chunky;
