036d849474d11e7d87185d2c35eefe74760ef52c
Christopher Bohn <pop4959@gmail.com>
2024-04-24 01:01:52 -0700

    Update gson lib

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,7 +1,7 @@
 dependencies {
     compileOnly(group = "com.google.code.gson", name = "gson", version = "2.8.9")
     testImplementation(group = "junit", name = "junit", version = "4.13.2")
-    testImplementation(group = "com.google.code.gson", name = "gson", version = "2.8.9")
+    testImplementation(group = "com.google.code.gson", name = "gson", version = "2.10.1")
     implementation(project(":chunky-nbt"))
 }
 
